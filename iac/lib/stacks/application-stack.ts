82ee2d52ef68b57e0f4da89341988e82c13a2761
Ethan Rucinski <ethan.rucinski@gmail.com>
2022-11-26 12:42:11 -0600

    Lambda function

--- iac/lib/stacks/application-stack.ts
+++ iac/lib/stacks/application-stack.ts
@@ -1,4 +1,5 @@
 import { StackProps } from "aws-cdk-lib";
+import { DockerImageCode, DockerImageFunction, FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { Dop302Bucket } from "../constructs/dop302-bucket";
 import { Dop302Stack } from "../constructs/dop302-stack";
@@ -8,5 +9,17 @@ export class ApplicationStack extends Dop302Stack {
     super(scope, id, props);
 
     const bucket = new Dop302Bucket(this, "Bucket");
+
+    const putObjectFunction = new DockerImageFunction(this, 'PutObjectFunction', {
+      code: DockerImageCode.fromImageAsset("../put-object"),
+      environment: {
+        BUCKET_NAME: bucket.bucketName
+      }
+    })
+
+    putObjectFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE
+    })
+    bucket.grantWrite(putObjectFunction)
   }
 }
---
+++ put-object/index.js
@@ -0,0 +1,35 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3")
+
+
+exports.handler = async(event, _) => {
+
+    // retrieve inputs
+    let body, key, content;
+    try {
+        body = JSON.parse(event.body);
+        key = body.key;
+        content = body.content;
+    } catch (err) {
+        return {
+            error: `Could not parse input ${err}`
+        }
+    }
+
+    try {
+        const clientS3 = new S3Client();
+        const  command = new PutObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Body: content,
+            Key: key
+        })
+        const result = await clientS3.send(command);
+        return result;        
+    } catch (err) {
+        return {
+            error: `Could not put object ${err}`,
+        };
+    }
+
+
+    
+}
