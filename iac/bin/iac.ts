36f933fbab3c69e746f14318e40b1eb4a2ec6c3d
dop301 <dop301@2KZ58.local>
2022-11-29 19:47:22 -0800

    no content to start

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -1,15 +1,5 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { VpcStack } from "../lib/stacks/vpc-stack";
-import { ApplicationStack } from "../lib/stacks/application-stack";
 
 const app = new cdk.App();
-
-new VpcStack(app, "VpcStack", {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-east-2" },
-});
-
-new ApplicationStack(app, "ApplicationStack", {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-east-2" },
-});
